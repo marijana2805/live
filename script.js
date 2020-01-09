64c3f332ca82a468ecad5dc50a641b7227411405
marijana2805 <masadjurnic@gmail.com>
2020-01-09 15:47:43 +0100

    druga verzija

--- script.js
+++ script.js
@@ -12,31 +12,5 @@ document.querySelectorAll('.top').forEach(item => {
         }
     })
 })
-/*document.getElementsByClassName("operator").onclick = function () {myFunction()};
-document.getElementById("demo").onclick = function () { myFunction()};
-function myFunction() {
-    if (document.getElementById("demo").style.display === "none") {
-        document.getElementById("demo").style.display="block";
-        document.getElementById("operator").innerText="-";
-    }else{ document.getElementById("demo").style.display="none";
-        document.getElementById("operator").innerText="+";
-    }
-}
-*/
 
-/*function myFunction() {
-    document.getElementById("operator").innerText = "+";
-}*/
-
-/*function myFunction() {
-    var x = document.getElementById("demo");
-    var y = document.getElementById("operator");
-    if (x.style.display === "none") {
-        x.style.display="block";
-        y.innerText="-";
-    }else{ x.style.display="none";
-           y.innerText="+";
-    }
-
-}*/
 
